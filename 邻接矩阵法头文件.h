c630151bba6244d50abdad57742a038e4efef268
foromly <150208864+foromly@users.noreply.github.com>
2023-11-08 18:23:44 +0800

    Update 邻接矩阵法头文件.h

--- 邻接矩阵法头文件.h
+++ 邻接矩阵法头文件.h
@@ -5,10 +5,11 @@
 #define MAX_NAME_LENGTH 100
 #define MAX_VERTICES 100
 
+
 // 边
 typedef struct {
     int length; // 边的长度
-    int type; // 边的类型(0是走路，1是坐车）
+    int type; // 边的类型(0是走路，1是坐车)
 } Edge;
 
 // 顶点
@@ -22,75 +23,83 @@ typedef struct {
     Vertex vertices[MAX_VERTICES]; // 顶点数组
     Edge edges[MAX_VERTICES][MAX_VERTICES]; // 边的二维数组
     int numVertices; // 顶点数量
-    int numEdge;//边的数量
-} Graph;
+    int numedge;//边的数量 
+} Graph,*map;
+
 
 // 初始化图
-void initializeGraph(Graph* graph) {
-    
+void initializeGraph(map& graph) {
+    graph->numVertices = 0;
+    graph->numedge = 0;
+    for (int i = 0; i < MAX_VERTICES; i++) {
+        for (int j = 0; j < MAX_VERTICES; j++) {
+            graph->edges[i][j].length = 0;
+            graph->edges[i][j].type = 0;
+        }
+    }
 }
 
+
 //增加一个新景点及其相关信息
-void addNewVertex(Graph* graph, const char* name, const char* info) {
+void addNewVertex(map& graph,char* name,char* info) {
     
 }
 
 // 从文件中读入景点
-void loadVerticesFromFile(Graph* graph, const char* filename) {
+void loadVerticesFromFile(map& graph, char* filename) {
     
 }
 
 // 输出景点到文件中
-void saveVerticesToFile(Graph* graph, const char* filename) {
+void saveVerticesToFile(map& graph, char* filename) {
     
 }
 
 
 // 增加一条新的路径
-void addNewEdge(Graph* graph, const char* start, const char* end, int length, int type) {
+void addNewEdge(map& graph, char* start, char* end, int length, int type) {
     
 }
 
 
 // 修改一个已有景点的相关信息
-void updateVertexInfo(Graph* graph, const char* name, const char* newInfo) {
+void updateVertexInfo(map& graph, char* name, char* newInfo) {
     
 }
  
 // 删除一个景点及其相关信息
-void removeVertex(Graph* graph, const char* name) {
+void removeVertex(map& graph, char* name) {
     
 }
 
 // 删除一条路径
-void removeEdge(Graph* graph, const char* start, const char* end) {
+void removeEdge(map& graph, char* start, char* end) {
     
 }
 
 
 // 校园游览线路
-void planTour(Graph* graph, const char* start, const char* wayPoint, const char* end) {
+void planTour(map& graph, char* start, char* wayPoint, char* end) {
    
 }
 
 
 // 增加“绕行”功能
-void avoidPath(Graph* graph, const char* avoidStart, const char* avoidEnd) {
+void avoidPath(map& graph, char* avoidStart, char* avoidEnd) {
     
 }
 
 
 // 查找顶点的索引
-int findVertexIndex(Graph* graph, const char* name) {
+int findVertexIndex(map& graph, char* name) {
     
 }
 
 // 查找最短路径和给出备用路径 
-void findShortestPath(Graph* graph, const char* start, const char* end) {
+void findShortestPath(map& graph, char* start, char* end) {
    
 
     
 }
 
 #endif /* GRAPH_H */
-
